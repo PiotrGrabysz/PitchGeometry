97e19a5338e65f78316af5793178e7f6449c6501
Piotr Grabysz <piotr.grabysz@veturai.com>
2023-01-12 21:14:51 +0100

    Fix test_metrics - now all tests pass

--- tests/models/test_metrics.py
+++ tests/models/test_metrics.py
@@ -5,100 +5,105 @@ import pytest
 
 from pitch_geo.models import metrics
 
-keypoints1 = np.array(
-    [[[[
-        532, 158, 1732, 227, 0, 0, 0, 0, 359, 203,
-        723, 234, 564, 300, 290, 410, 0, 0, 0, 0,
-        174, 250, 300, 264, 0, 0, 0, 0, 276, 336,
-        1718, 383, 1712, 474, 1702, 596, 561, 358, 1332, 443,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0
-    ]]]]
-)
-visibility1 = np.array(
+x = [
+    532,
+    158,
+    1732,
+    227,
+    0,
+    0,
+    0,
+    0,
+    359,
+    203,
+    723,
+    234,
+    564,
+    300,
+    290,
+    410,
+    0,
+    0,
+    0,
+    0,
+]
+y = [
+    174,
+    250,
+    300,
+    264,
+    0,
+    0,
+    0,
+    0,
+    276,
+    336,
+    1718,
+    383,
+    1712,
+    474,
+    1702,
+    596,
+    561,
+    358,
+    1332,
+    443,
+]
+vis = [1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+keypoints1 = np.expand_dims(np.array([x, y, vis]).T, axis=0)
+
+tp_ground_truth = np.array([[[532, 1732, 1], [158, 227, 1], [0, 0, 0]]])
+
+tp_pred1 = np.array([[[532, 1732, 1], [158, 227, 1], [10, 10, 1]]])
+tp_pred2 = np.array([[[532, 0, 1], [158, 0, 1], [0, 0, 0]]])
+tp_pred3 = np.array([[[0.05, 0.05, 1], [0, 0, 0], [10, 10, 1]]])
+tp_pred4 = np.array([[[0.05, 0.05, 1], [0, 0, 0], [0, 0, 0]]])
+
+
+@pytest.mark.parametrize(
+    "kps_true, kps_pred, precision",
     [
-        False, False, True, True, False, False, False, False, True,
-        True, False, False, True, True, False, False, False, False,
-        False, False, True, True, True, True, True, True, True,
-        True, True, True, True, True, True, True, True, True,
-        True, True
+        (keypoints1, keypoints1, 1.0),  # identical keypoint should have precision = 1.0
+        (tp_ground_truth, tp_pred1, 0.66666666),  # two TP, one FP
+        (tp_ground_truth, tp_pred2, 1.0),  # two TP, zero FP
+        (tp_ground_truth, tp_pred3, 0.5),  # one TP one FP
+        (tp_ground_truth, tp_pred4, 1.0),  # one TP, zero FP
     ],
-    dtype=bool
 )
-
-
-@pytest.mark.parametrize('x, vis', [
-    (keypoints1, visibility1)
-])
-def test_not_visible(x, vis):
-    assert np.array_equal(metrics.not_visible(keypoints1), vis)
-
-
-tp_ground_truth = np.array(
-    [[[[
-        532, 158, 1732, 227, 0, 0, 0, 0
-    ]]]]
-)
-tp_pred1 = np.array(
-    [[[[
-        532, 158, 1732, 227, 10, 10, 10, 10
-    ]]]]
-)
-tp_pred2 = np.array(
-    [[[[
-        532, 158, 0, 0, 0, 0, 0, 0
-    ]]]]
-)
-tp_pred3 = np.array(
-    [[[[
-        0.05, 0.05, 0, 0, 0, 0, 10, 10
-    ]]]]
-)
-tp_pred4 = np.array(
-    [[[[
-        0.05, 0.05, 0, 0, 10, 10, 10, 10
-    ]]]]
-)
-
-
-@pytest.mark.parametrize('kps_true, kps_pred, precision', [
-    (keypoints1, keypoints1, 1.0),  # identical keypoint should have precision = 1.0
-    (tp_ground_truth, tp_pred1, 0.0),   # no TP
-    (tp_ground_truth, tp_pred2, 0.66666666),  # two TP and one FP
-    (tp_ground_truth, tp_pred3, 0.33333333),  # one TP and two FP
-    (tp_ground_truth, tp_pred4, 0)  # zero TP
-])
 def test_precision_metric(kps_true, kps_pred, precision):
-    m = metrics.NotVisiblePrecision(name='test_not_visible_precision_metric')
+    m = metrics.VisiblePrecision(name="test_not_visible_precision_metric")
     m.update_state(kps_true, kps_pred)
     assert np.isclose(m.result().numpy(), precision)
 
 
-@pytest.mark.parametrize('kps_true, kps_pred, precision', [
-    (tp_ground_truth, tp_pred1, 0.0),   # no TP
-    (tp_ground_truth, tp_pred2, 0.66666666),  # two TP and one FP
-    (tp_ground_truth, tp_pred3, 0.33333333),  # one TP and two FP
-    (tp_ground_truth, tp_pred4, 0)  # zero TP
-])
+@pytest.mark.parametrize(
+    "kps_true, kps_pred, precision",
+    [
+        (tp_ground_truth, tp_pred1, 0.66666666),  # two TP, one FP
+        (tp_ground_truth, tp_pred2, 1.0),  # two TP, zero FP
+        (tp_ground_truth, tp_pred3, 0.5),  # one TP one FP
+        (tp_ground_truth, tp_pred4, 1.0),  # one TP, zero FP
+    ],
+)
 def test_precision_metric2(kps_true, kps_pred, precision):
-    m = metrics.NotVisiblePrecision()
-    m.update_state(kps_true[:, :, :, :2], kps_pred[:, :, :, :2])
-    m.update_state(kps_true[:, :, :, 2:4], kps_pred[:, :, :, 2:4])
-    m.update_state(kps_true[:, :, :, 4:6], kps_pred[:, :, :, 4:6])
-    m.update_state(kps_true[:, :, :, 6:], kps_pred[:, :, :, 6:])
+    m = metrics.VisiblePrecision()
+    m.update_state(np.expand_dims(kps_true[:, 0, :], axis=0), np.expand_dims(kps_pred[:, 0, :], axis=0))
+    m.update_state(np.expand_dims(kps_true[:, 1, :], axis=0), np.expand_dims(kps_pred[:, 1, :], axis=0))
+    m.update_state(np.expand_dims(kps_true[:, 2, :], axis=0), np.expand_dims(kps_pred[:, 2, :], axis=0))
     assert np.isclose(m.result().numpy(), precision)
 
 
-@pytest.mark.parametrize('kps_true, kps_pred, precision', [
-    (keypoints1, keypoints1, 1.0),  # identical keypoint should have recall = 1.0
-    (tp_ground_truth, tp_pred1, 0.0),
-    (tp_ground_truth, tp_pred2, 1.0),  # two TP
-    (tp_ground_truth, tp_pred3, 0.5),   # one TP and one FN
-    (tp_ground_truth, tp_pred4, 0.0)
-])
+@pytest.mark.parametrize(
+    "kps_true, kps_pred, precision",
+    [
+        (keypoints1, keypoints1, 1.0),  # identical keypoint should have recall = 1.0
+        (tp_ground_truth, tp_pred1, 1.0),
+        (tp_ground_truth, tp_pred2, 1.0),
+        (tp_ground_truth, tp_pred3, 0.5),
+        (tp_ground_truth, tp_pred4, 0.5),
+    ],
+)
 def test_recall_metric(kps_true, kps_pred, precision):
-    m = metrics.NotVisibleRecall()
+    m = metrics.VisibleRecall()
     m.update_state(kps_true, kps_pred)
     assert np.isclose(m.result().numpy(), precision)
